09356791a104105b7051d9716f96e6ad0442ad9f
saenal <saenal.cha@yanolja.com>
2020-09-09 23:49:05 +0900

    end

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 import { HashRouter, Route } from 'react-router-dom'
 import About from './routes/About'
 import Home from './routes/Home'
+import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
+
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
-      <Route path="/about" compoennt={About} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   )
 }
